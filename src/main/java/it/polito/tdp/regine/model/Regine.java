07aa848afcb9d204613ed9d9f307b4b4768b3288
Roberta Bottazzi <s261573@studenti.polito.it>
2021-04-12 19:48:02 +0200

    Soluzione con la prima soluzione valida

--- src/main/java/it/polito/tdp/regine/model/Regine.java
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -1,8 +1,16 @@
 package it.polito.tdp.regine.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Regine {
+	
+	public List<Integer> regine (int N){
+		List<Integer> risultato= new ArrayList<>();
+		cerca(new ArrayList<Integer>(),N);
+		return risultato;
+		
+	}
 
 	// N è il numero di righe e colonne della scacchiera
 	//   (righe e colonne numerate da 0 a N-1)
@@ -17,15 +25,56 @@ public class Regine {
 	//     [0, 2]
 	//            [0, 2, 1]
 	
-	private void cerca(List<Integer>parziale, int livello) {
+	private int N;
+	private List<Integer> soluzione;
+	
+	public List<Integer> risolvi(int N){  //procedura che attiva la chiamata alla procedura ricorsiva 
+		this.N=N;
+		//caso iniziale
+		List<Integer> parziale= new ArrayList<Integer>();
+		this.soluzione=null;
+		cerca(parziale,0);
+		return this.soluzione;		
+	}
+	
+	private boolean cerca(List<Integer>parziale, int livello) {
 		if(livello==N) {
+			this.soluzione=new ArrayList<>(parziale);  //this.soluzione.addAll(parziale);  è necessario perchè alla fine per via del backtracking parziale è sempre vuota
+			return true;
 			// caso terminale
 		} else {
-			for(int colonna=0; colonna<N; colonna++) {
-				// if la possa nella casella [livello][colonna] è valida
+			for(int colonna=0; colonna<N; colonna++) { //questo ciclo mi permette di fare più tentativi, quindi io provo tutte le posizioni possibili ma valide
+				// if la mossa nella casella [livello][colonna] è valida
 				// se sì, aggiungi a parziale e fai ricorsione
+				//questo è uno schema di ricorsione STANDARD: 
+				//aggiungo, provo, tolgo
+				if(posValida(parziale,colonna)) { //faccio ricorsione se la colonna in cui voglio inserire la regina è compatibile con quelle precedenti
+					parziale.add(colonna);  //ho aggiunto la colonna perchè rispetta tutti i vincoli
+					boolean trovato=cerca(parziale,livello+1); //vedo le conseguenze di aver messo la regina nella colonna sulle righe successive
+					if(trovato)
+						return true; //così facendo non va avanti a trovare altre soluzioni
+					parziale.remove(parziale.size()-1);  //backtracking, se il tentativo non va a buon fine con quella colonna, perciò riprovo con le altre colonne possibili finchè non trovo la soluzione finale
+				}
 			}
+			return false;
 		}
+	}
+
+	private boolean posValida(List<Integer> parziale, int colonna) {
+		int livello=parziale.size();
+		//controlla se la regina viene mangiata in verticale (in orizzontale per costruzione non può essere mangiata)
+		if(parziale.contains(colonna))
+			return false;
+		//controlla se la regina viene mangiata in diagonale
+		//le diagonali hanno la proprietà che riga+colonna=numero costante, quindi prendo le regine posizionate e se la somma (o la differenza) delle loro righe e colonne è uguale alla posizione della regina che sto per posizionare non va bene
+		for(int r=0; r<livello;r++) {
+			int c=parziale.get(r);
+			
+			if(r+c==livello+colonna || r-c==livello-colonna)
+				return false;
+		}
+		
+		return true;
 	}
 	
 	
